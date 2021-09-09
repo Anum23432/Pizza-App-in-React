faabd56529c6bcff17126ac4193d8f148166d474
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-09-09 20:07:22 +0500

    Pizza App

--- src/components/CancelOrder.js
+++ src/components/CancelOrder.js
@@ -1,32 +1,59 @@
-import React from 'react';
 import swal from 'sweetalert';
+import React, {useEffect, useState} from 'react';
 
-function CancelOrder(){
-    return(
-        <>
-   
-        <div id="app-cover">
-      <input onClick = {() => cancelOrder()} type="checkbox" id="checkbox"/>
-      <div id="bin-icon">
-        <div id="lid"></div>
-        <div id="box">
-          <div id="box-inner">
-            <div id="bin-lines"></div>
-          </div>
-        </div>
+function CancelOrder() {
+  const [orders,setOrders] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    fetch(`https://order-pizza-api.herokuapp.com/api/orders`, requestOptions)
+        .then(response => response.json())
+        .then(data => setOrders(data))
+    },
+ []);
+
+ const onClickCancel = (Order_ID) => {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    fetch(`https://order-pizza-api.herokuapp.com/api/orders/${Order_ID}`, requestOptions)
+    .then(response => {
+        if (response.status === 200) {
+            swal('Cancelled successfully')
+        }
+        window.location.reload();
+    })
+ }
+
+  return (
+    <>
+    <div>{orders.map((v,i) => {
+      return <div key={i}>
+      <div class="card mb-3 mx-auto" style={{width: '200px', margin: '45px', padding:'15px',height:'200px', backgroundColor:'wheat'}}>
+      <div class="row g-0" >
+      <div class="col-md-8">
+      <div className="card-body">
+        <div className='card-title'>Crust: {v.Crust} </div>
+        <div className='card-subtitle'>Flavor: {v.Flavor} </div>
+        <div className='card-text'>Size: {v.Size} </div>
+        <button className="btn btn-danger" style={{marginLeft:'5%'}} onClick={() => onClickCancel(v.Order_ID)}>CancelOrder</button>
       </div>
-      <div id="layer"></div>
+      </div>
+      </div>
+      </div>
+                <br />
+                <br />
+             </div>})}
     </div>
-       </>
     
-    
-    )}
-    
-    function cancelOrder() {
-        swal("Order Cancelled!");
-    }
-
-    
+    </>
+  );
+}
 
 
 export default CancelOrder;
