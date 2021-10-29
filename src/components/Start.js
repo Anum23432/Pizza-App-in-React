cafe6293f661a2381a08311377f9845298ed8f28
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-10-29 20:47:46 +0500

    pizza app

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import {useHistory} from 'react-router';
-
+import Final from './Final';
 
 function Start(){
     let history = useHistory()
     return(
         <>
+        
         <center>
+        <div className="heading">
+							<h1>Feeling Hungry..? Order Now</h1>
+	                       </div>
         <div className="btn">
          <button className="btn btn-danger"  onClick={() => history.push('./ViewOrder')}>ViewOrder</button>
          <button className="btn btn-danger"  onClick={() => history.push('./PlaceOrder')}>PlaceOrder</button>
          <button  className="btn btn-danger" onClick={() => history.push('./CancelOrder')}>CancelOrder</button>
          </div>
-         </center><br/>
+         </center><br/><br/><br/><br/>
         
          
-         <div id="pizza" className="hidden-md-down">
-							<div className="bord">
-								<div className="bord-rand"></div>
-									<div className="bord-binnen"></div>
-								</div>
-								
-								<div className="pizzasnede">
-									<div className="korst"></div>
-									<div className="kaas"></div>
-									<div className="salami een vallen"></div>
-									<div className="salami-half twee vallen"></div>
-									<div className="salami drie vallen"></div>
-								</div>
-							</div> 
+        
+         <Final/>
                             
-                            
+          
+                                          
         </>
 
     )
